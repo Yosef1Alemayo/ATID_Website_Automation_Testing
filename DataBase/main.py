8cee430a2bf542dda5dffd31dd8b416c64da3766
Your Name <you@example.com>
2022-06-27 16:47:13 +0300

    Add DB for the practice, and API tests

--- DataBase/main.py
+++
@@ -1,8 +0,0 @@
-from mySql import DB_Connect
-
-''' This Query is for example, There is no DataBase in The Website .. 
- The Website is for Practice To QA '''
-
-query1 = DB_Connect('localhost', 'root', '******', '3306', 'sakila')
-
-query1.execute('select * from actor where first_name = "KIRK" ')
--- Pages/aboutUs_page.py
+++
@@ -1,7 +0,0 @@
-
-class AboutUs_Page:
-    def __init__(self, driver):
-        self.driver = driver
-
-
-
